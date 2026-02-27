7644b42d655561c8ef6e03d488aed859b93e0d7d
terra <mordeworti@gmail.com>
2026-02-26 21:00:40 -0500

    Your commit message here

--- src/routes/patients.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-const { Router } = require('express');
-const patientController = require('../controllers/patientController');
-const router = Router();
-
-router.get('/:email/history', patientController.getHistory);
-
-module.exports = router;
