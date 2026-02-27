98e48a6f5fbba450c6727582a4d75c207b0b7cf7
terra <mordeworti@gmail.com>
2026-02-27 00:08:34 -0500

    Add front-end implementation

--- src/routes/reports.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-const { Router } = require('express');
-const reportController = require('../controllers/reportController');
-const router = Router();
-
-router.get('/revenue', reportController.getRevenue);
-
-module.exports = router;
